584ed3a1017e69ca0bdb5d25da75da570e81cc51
Jan Storz <jan.storz@gmail.com>
2024-03-27 09:50:13 +0100

    structure redesign. Not finished yet

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from streamlit_extras.app_logo import add_logo
 NUM_CLASSES = 10
 
 st.set_page_config(page_title="SignLens Demo",
-                   page_icon="🎭", layout="wide",
+                   page_icon="⚙︎", layout="wide",
                    initial_sidebar_state="expanded")
 
 # Add custom CSS
@@ -29,85 +29,177 @@ st.markdown("""
     padding-bottom: 1rem;
 }
 
-.stButton > button:hover {
-    background-color: #4caf50;
-    color: white;
+h1 {
+    color: #8968CD;
 }
 </style>
 """, unsafe_allow_html=True)
 
+# .stButton > button:hover {
+#     background-color: #c91a2c; #4caf50;
+#     color: purple;
+# }
+
+
+# Initialize the key in session state
+if 'clicked' not in st.session_state:
+    st.session_state.clicked = {1:False,2:False}
+
+# Function to update the value in session state
+def clicked(button):
+    '''Update the value in session state when a button is clicked.'''
+    st.session_state.clicked[button] = True
+
 # Sidebar content
 logo = "https://img.freepik.com/free-photo/sign-language-collage-design_23-2150528183.jpg?t=st=1711466807~exp=1711470407~hmac=c1c1a9a378d0a17254e6cf298fb262c2883e305f2ee08999e0771f76be98eeb4&w=900"
 #logo = "https://www.freepik.com/free-vector/technology-circle-ai-abstract-vector-computer-vision-design_18236528.htm#query=cyborg%20eye&position=6&from_view=keyword&track=ais&uuid=6aae1df3-0c6e-49d5-a59f-300d5c4bd73d"
-#logo = "signlens-high-resolution-logo.png"
-#st.sidebar.image(logo, width=200)
-add_logo(logo_url=logo)
+logo = "signlens-high-resolution-logo.png"
+st.sidebar.image(logo,use_column_width=True)
+#add_logo(logo_url=logo,height=10)
 st.sidebar.title("About SignLens")
-st.sidebar.caption("An app for sign language translation using Mediapipe and an RNN model.")
+st.sidebar.caption("An app for translating sign language, but also aid in learning it.")
+st.sidebar.info("This is a demo app using computer vision and deep learning. Upload a video of sign language gestures and click the button to translate the signs to text. The app uses the Mediapipe library to extract landmarks from the video frames and sends the landmarks to a FastAPI server running a pre-trained RNN model to predict the sign language word.")
+#placeholder = st.empty()
 
+st.title("SignLens Demo")
 # Main content
-col1, col2 = st.columns([1, 2])
+col1, col2 = st.columns([2, 1])
+
+st.session_state.b2_disabled = True
 
 with col1:
-    st.title("SignLens Demo")
-    st.subheader("Sign Language translation")
+    st.subheader("Upload a video to translate sign to text")
+    holder = st.empty()
+    title1 = st.empty()
+    # Open video file if uploaded
+    #if st.button("Upload a video", on_click=clicked, args=[1], key="upload_video",type="primary"):
+    with st.spinner("Uploading video..."):
+        #time.sleep(1.5)
+        video = st.file_uploader("Select video file", label_visibility="collapsed", key="upload_video", help="Upload a video file",
+                                 #disabled=~st.session_state.b2_disabled,
+                                 type=["mp4", "mov", "avi", "mkv",
+                                                         #"m4v", "mkv", "wmv", "flv", "webm", "3gp", "ogg", "ogv", "gif", "mpg", "mpeg",
+                                                         "asf", "m2v", "ts", "m2ts", "mts", "vob"], accept_multiple_files=False)
+    if video:
+        st.session_state.b2_disabled = False
+        holder.video(video)
+        title1.write(":green[Video uploaded successfully!] :tada:")
 
 with col2:
-    # Open video file if uploaded
-    video = st.file_uploader("Upload a video", type=["mp4", "mov", "avi", "mkv",
-                                                         "m4v", "mkv", "wmv", "flv", "webm", "3gp", "ogg", "ogv", "gif", "mpg", "mpeg",
-                                                         "asf", "m2v", "ts", "m2ts", "mts", "vob"], accept_multiple_files=False)
+    st.subheader("Sign translation")
+    #expander = st.expander("Optional controls")
+    #expander.radio("Options", ["Translate", "Learn", "Live"])
+    #if st.button("Push to translate"):
+    # Add a button to start the prediction
+    # Conditional based on value in session state, not the output
+    st.button("Start Translation", on_click=clicked, args=[2],
+              type="primary", disabled=st.session_state.b2_disabled)
+    if video: #st.session_state.clicked[1]:
+        if st.session_state.clicked[2]:
+            state = "running"
 
-if video:
-    # Display video preview
-    st.video(video)
+            # Create a status container
+            status_text = st.empty()
 
-    state = "running"
+            try:
+                start_time = time.time()
 
-    # Create a status container
-    status_text = st.empty()
+                with st.spinner("⏱️ ... 🐢"):
+                    # Display the status
+                    status_text.text("Extracting landmarks")
 
-    try:
-        start_time = time.time()
+                    # Call the process_video_to_landmarks_json function
+                    json_landmarks = process_video_to_landmarks_json(video)
 
-        with st.spinner("⏱️ ... 🐢"):
-            # Display the status
-            status_text.text("Extracting landmarks")
+                    # st.json(json_landmarks, expanded=False) # just for debugging
 
-            # Call the process_video_to_landmarks_json function
-            json_landmarks = process_video_to_landmarks_json(video)
+                with st.spinner("⏱️ ... 🐢"):
+                    status_text.text("Requesting API response...")
 
-            st.json(json_landmarks, expanded=False)
+                    headers = {'Content-Type': 'application/json'}
+                    response = requests.post(#"http://127.0.0.1:8000/predict",
+                                            'https://signlens-pait7pkgma-oa.a.run.app/predict',
+                                            headers=headers, json=json_landmarks, timeout=120)
 
-        with st.spinner("Requesting API response..."):
-            headers = {'Content-Type': 'application/json'}
-            response = requests.post(#"http://127.0.0.1:8000/predict",
-                                     'https://signlens-pait7pkgma-oa.a.run.app/predict',
-                                     headers=headers, json=json_landmarks, timeout=120)
+                elapsed_time = time.time() - start_time
 
-        elapsed_time = time.time() - start_time
+                # Check the response code and handle accordingly
+                if response.ok:  # 200 <= response.status_code < 300
+                    status_text.text(f"Video processing complete! 🎉 (Elapsed time: {elapsed_time:.2f} seconds)")
+                    result = response.json()
+                    sign = result['sign']
+                    proba = result['probability']
+                    proba = float(list(proba)[0])
+                    #st.write(result['sign'])
+                    #st.success(f"Our model predicts the following sign: {result['sign']} with a probability of {100*(result['probability']):.2f}%")
+                    #st.write(f"sign: {result['sign:']}")
+                    #st.metric("probability", f"{100*(result['probability']):.2f}%")
+                    state = "complete"
+                    time.sleep(1)
+                    st.balloons()
 
-        # Check the response code and handle accordingly
-        if response.ok:  # 200 <= response.status_code < 300
-            status_text.text(f"Video processing complete! 🎉 (Elapsed time: {elapsed_time:.2f} seconds)")
-            result = response.json()
-            st.success(f"Result: {result}")
-            st.write(f"Word: {result['Word:']}")
-            st.write(f"Probability: {result['Probability:']}")
-            state = "complete"
-            st.balloons()
-        else:
-            status_text.text(f"API Error: {response.status_code}")
-            st.error(f"API Error: {response.status_code}")
-            state = "error"
+                    search_term = result['sign']
+                    # Pixabay API
+                    api_key = str(st.secrets.api_key)
+                    search_term_str = '+'.join(str(x) for x in search_term)
+                    api_key
+                    url = "https://pixabay.com/api/?key=%s&q=%s&image_type=photo&pretty=true" % (str(api_key),search_term_str)
+                    response = requests.get(url, timeout=60)
+                    image_data = response.json()
+                    #image_data
+                    img_url = image_data["hits"][0]["webformatURL"]
 
-    except Exception as e:
-        status_text.text(f"API Error: {e}")
-        st.error(f"API Error: {e}")
-        state = "error"
+                    # Lookup an image of the result['sign'] using Unsplash API
+                    #unsplash_access_key = "YOUR_UNSPLASH_ACCESS_KEY"
+                    #url = f"https://api.unsplash.com/photos/random/?query={result['sign']}&client_id={unsplash_access_key}"
+                    # response = requests.get(url)
+                    # image_data = response.json()
+                    # image_url = image_data["urls"]["small"]
 
+                    # Lookup a GIF of the result['sign'] using Tenor API
+                    # ckey = "signlens_app"
+                    # tenor_access_key = "AIzaSyAJPjaf0y_8RtvPm-xJ5xsitGDDy-oAfmc"
+                    # url = "https://tenor.googleapis.com/v2/search?q=%s&key=%s&client_key=%s&limit=%s" % (search_term, tenor_access_key, ckey,  1)
 
+                    # response = requests.get(url, timeout=60)
+                    # gif_data = response.json()
+                    # gif_data
+                    # gif_url = gif_data["results"][0]["media_formats"]["gif"]["url"]
 
+#                     api_key ="AIzaSyAJPjaf0y_8RtvPm-xJ5xsitGDDy-oAfmc"
+# # GET https://customsearch.googleapis.com/customsearch/v1?imgSize=SMALL&q=glasswindow&safe=medium&searchType=image&key=[YOUR_API_KEY] HTTP/1.1
+#                     url = f"https://www.googleapis.com/customsearch/v1?imgSize=SMALL&q={search_term}&safe=medium&searchType=image&key={api_key}"
+#                     response = requests.get(url)
+#                     image_data = response.json()
+#                     img_url = image_data["items"][0]["link"]
+
+                    #"Translation guess:"
+                    #st.header(f"{result['sign']}")
+
+                    probability_color = "green" if 70*(proba) > 100 else "yellow" if 50*(proba) < 100 else "red"
+                    probc = ":%s[%s]" % (probability_color, f"{100*(proba):.2f}%")
+                    #st.metric("probability",f"{100*(proba):.2f}%", probability_color)
+                    st.metric("Translation guess", f"{result['sign']}")
+                    probability_color
+                    st.subheader("Probability", f":green[{proba}]")
+                    if probability_color == "green":
+                        st.subheader("Probability", f":green[{100*({proba}):.2f}%]")
+                    elif probability_color == "yellow":
+                        st.subheader("Probability", f":yellow[{100*({proba}):.2f}%]")
+                    else:
+                        st.subheader("Probability", f":red[{100*({proba}):.2f}%]")
+                    st.image(img_url)
+                        #st.image(gif_url)
+
+                else:
+                    status_text.text(f"API Error: {response.status_code}")
+                    st.error(f"API Error: {response.status_code}")
+                    state = "error"
+
+            except Exception as e:
+                status_text.text(f"API Error: {e}")
+                st.error(f"API Error: {e}")
+                state = "error"
 
 
 
