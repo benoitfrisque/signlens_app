f368b2097d667ade8e1871350233c281570121f9
Jan Storz <jan.storz@gmail.com>
2024-03-27 12:00:32 +0100

    redesigning and funcitoning coloring of proba and lookup of image

--- app.py
+++ app.py
@@ -6,15 +6,72 @@ import cv2
 from video_utils import process_video_to_landmarks_json
 from streamlit_extras.app_logo import add_logo
 
-NUM_CLASSES = 10
+import base64
+
 
 st.set_page_config(page_title="SignLens Demo",
                    page_icon="⚙︎", layout="wide",
                    initial_sidebar_state="expanded")
 
+
+# @st.cache_resource(allow_output_mutation=True)
+# def get_base64_of_bin_file(bin_file):
+#     with open(bin_file, 'rb') as f:
+#         data = f.read()
+#     return base64.b64encode(data).decode()
+
+# def set_png_as_page_bg(png_file):
+#     bin_str = get_base64_of_bin_file(png_file)
+#     page_bg_img = '''
+#     <style>
+#     body {
+#     background-image: url("data:image/png;base64,%s");
+#     background-size: cover;
+#     }
+#     </style>
+#     ''' % bin_str
+
+#     st.markdown(page_bg_img, unsafe_allow_html=True)
+#     return
+
+# set_png_as_page_bg('resources/background_signlens.png')
+
+NUM_CLASSES = 10
+
+# def set_background(png_file):
+#     bin_str = get_base64(png_file)
+#     page_bg_img = '''
+#     <style>
+#     .stApp {
+#     background-image: url("data:image/png;base64,%s");
+#     background-size: cover;
+#     }
+#     </style>
+#     ''' % bin_str
+#     st.markdown(page_bg_img, unsafe_allow_html=True)
+# set_background('resources/background_signlens.png')
+
+# url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
+
+# @st.cache_data
+# def get_img_as_base64(file):
+#     with open(file, "rb") as f:
+#         data = f.read()
+#     return base64.b64encode(data).decode()
+
+# img_background = get_img_as_base64("resources/background_signlens.png")
+
+#https://drive.google.com/file/d/1kzhZnny42X52bbAehiN94USuLKowmiJk/view?usp=drive_link
+#https://1drv.ms/i/s!AokgWCkazMPtpiqzTu31KLHslEj-?e=HNTSKG
 # Add custom CSS
 st.markdown("""
 <style>
+[data-testid="stAppViewContainer"] > .main {
+    background-image: url("resources/background_signlens.png");
+    background-size: 100vw 100vh;  # This sets the size to cover 100% of the viewport width and height
+    background-position: center;
+    background-repeat: no-repeat;
+
 .reportview-container .main .block-container {
     padding-top: 1rem;
     padding-right: 2.5rem;
@@ -31,6 +88,12 @@ st.markdown("""
 
 h1 {
     color: #adffb7;
+}
+<!--
+body {
+background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
+background-size: cover;
+-->
 }
 </style>
 """, unsafe_allow_html=True)
@@ -53,7 +116,7 @@ def clicked(button):
 # Sidebar content
 logo = "https://img.freepik.com/free-photo/sign-language-collage-design_23-2150528183.jpg?t=st=1711466807~exp=1711470407~hmac=c1c1a9a378d0a17254e6cf298fb262c2883e305f2ee08999e0771f76be98eeb4&w=900"
 #logo = "https://www.freepik.com/free-vector/technology-circle-ai-abstract-vector-computer-vision-design_18236528.htm#query=cyborg%20eye&position=6&from_view=keyword&track=ais&uuid=6aae1df3-0c6e-49d5-a59f-300d5c4bd73d"
-logo = "signlens-high-resolution-logo.png"
+logo = "resources/signlens-high-resolution-logo-transparent.png"
 st.sidebar.image(logo,use_column_width=True)
 #add_logo(logo_url=logo,height=10)
 st.sidebar.title("About SignLens")
@@ -104,17 +167,17 @@ with col2:
             try:
                 start_time = time.time()
 
-                with st.spinner("⏱️ ... 🐢"):
+                with st.spinner("⏱️ Extracting landmarks... 🐢"):
                     # Display the status
-                    status_text.text("Extracting landmarks")
+                    #status_text.status("Extracting landmarks")
 
                     # Call the process_video_to_landmarks_json function
                     json_landmarks = process_video_to_landmarks_json(video)
 
                     # st.json(json_landmarks, expanded=False) # just for debugging
 
-                with st.spinner("⏱️ ... 🐢"):
-                    status_text.text("Requesting API response...")
+                with st.spinner("⏱️ Requesting API response... 🐢"):
+                    #status_text.status("Requesting API response...")
 
                     headers = {'Content-Type': 'application/json'}
                     response = requests.post(#"http://127.0.0.1:8000/predict",
@@ -125,7 +188,7 @@ with col2:
 
                 # Check the response code and handle accordingly
                 if response.ok:  # 200 <= response.status_code < 300
-                    status_text.text(f"Video processing complete! 🎉 (Elapsed time: {elapsed_time:.2f} seconds)")
+                    status_text.text(f"Video processing complete! 🎉     (Elapsed time: {elapsed_time:.2f} seconds)")
                     result = response.json()
                     #result
                     sign = result['sign']
@@ -139,16 +202,33 @@ with col2:
                     time.sleep(1)
                     st.balloons()
 
+                    #"Translation guess:"
+                    #st.header(f"{result['sign']}")
+
+                    # probc = ":%s[%s]" % (probability_color, f"{100*(proba):.2f}%")
+                    # #st.metric("probability",f"{100*(proba):.2f}%", probability_color)
+                    st.title(sign)
+                    #st.write(:green["Translation guess"])
+                    #probability_color
+                    #"probability:"
+                    #st.metric(value=proba,label="probability")
+                    #st.text_area("Translation guess", sign, height=100)
+                    probability_color = "green" if proba > 50 else "yellow" if proba < 80 else "red"
+                    if probability_color == "green":
+                        st.write("is our best guess with probability", f":green[{proba}%]")
+                    elif probability_color == "yellow":
+                        st.write("is our best guess with probability", f":yellow[{proba}%]")
+                    else:
+                        st.write("is our best guess with probability", f":red[{proba}%]")
+
                     search_term = result['sign']
                     # Pixabay API
-                    # api_key = str(st.secrets.api_key)
-                    # search_term_str = '+'.join(str(x) for x in search_term)
-                    # api_key
-                    # url = "https://pixabay.com/api/?key=%s&q=%s&image_type=photo&pretty=true" % (str(api_key),search_term_str)
-                    # response = requests.get(url, timeout=60)
-                    # image_data = response.json()
-                    # #image_data
-                    # img_url = image_data["hits"][0]["webformatURL"]
+                    api_key = str(st.secrets.api_key)
+                    url = "https://pixabay.com/api/?key=%s&q=%s&image_type=photo&pretty=true" % (str(api_key),search_term)
+                    response = requests.get(url, timeout=60)
+                    image_data = response.json()
+                    #image_data
+                    img_url = image_data["hits"][0]["webformatURL"]
 
                     # Lookup an image of the result['sign'] using Unsplash API
                     #unsplash_access_key = "YOUR_UNSPLASH_ACCESS_KEY"
@@ -173,24 +253,7 @@ with col2:
 #                     response = requests.get(url)
 #                     image_data = response.json()
 #                     img_url = image_data["items"][0]["link"]
-
-                    #"Translation guess:"
-                    #st.header(f"{result['sign']}")
-
-                    # probability_color = "green" if 70*(proba) > 100 else "yellow" if 50*(proba) < 100 else "red"
-                    # probc = ":%s[%s]" % (probability_color, f"{100*(proba):.2f}%")
-                    # #st.metric("probability",f"{100*(proba):.2f}%", probability_color)
-                    st.metric("Translation guess", f"{result['sign']}")
-                    # probability_color
-                    "probability:"
-                    st.metric(f"{proba}")
-                    # if probability_color == "green":
-                    #     st.subheader("Probability", f":green[{100*({proba}):.2f}%]")
-                    # elif probability_color == "yellow":
-                    #     st.subheader("Probability", f":yellow[{100*({proba}):.2f}%]")
-                    # else:
-                    #     st.subheader("Probability", f":red[{100*({proba}):.2f}%]")
-                    # st.image(img_url)
+                    st.image(img_url)
                         #st.image(gif_url)
 
                 else:
