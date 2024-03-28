9bff4fd2fde77b1b6e58df3167c759585c9245c6
Benoit Frisque <benoitfrisque@gmail.com>
2024-03-28 06:51:26 +0100

    pages/Game.py

--- pages/Game.py
+++ pages/Game.py
@@ -3,21 +3,20 @@ import os
 import numpy as np
 import pandas as pd
 import time
+import random
 
 st.set_page_config(
-        page_title="SignLens Play",
-        page_icon="resources/signlens-favicon-black.png",
-        layout="wide",
-        initial_sidebar_state="expanded",
-        menu_items={
-            'Report a bug': "https://github.com/benoitfrisque/signlens",
-            'About': "# This is our final project for Le Wagon Data Science Bootcamp!"
-            }
+    page_title="SignLens Play",
+    page_icon="resources/signlens-favicon-black.png",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Report a bug': "https://github.com/benoitfrisque/signlens",
+        'About': "# This is our final project for Le Wagon Data Science Bootcamp!"
+    }
 )
 
-
 st.title("ASL Learning Game")
-
 
 # Load CSV data
 @st.cache_data
@@ -36,65 +35,113 @@ data = load_learn_video_url_list()
 # Filter unique signs
 unique_signs = np.sort(data['sign'].unique())
 
-@st.cache_data
-def select_random_video():
-    random_index = np.random.randint(len(data))
-    return data.iloc[random_index]
 
-@st.cache_data
-def generate_options(unique_signs, correct_option):
-    options = np.random.choice(unique_signs, size=3, replace=False)
-    options = np.append(options, correct_option)
-    np.random.shuffle(options)
-    return options
+# callback function to change the random number stored in state
+def select_random_video():
+    st.session_state.random_index = np.random.randint(len(data))
+
+    random_video_data = data.iloc[st.session_state.random_index]
+    st.session_state.url = random_video_data['url']
+
+    st.session_state.correct_option = random_video_data['sign']
+    # Randomly choose options
+    options = np.random.choice(unique_signs, size=4, replace=False)  # Choose 3 random options
+
+    # Concatenate correct option and randomly chosen options
+    options = np.append(st.session_state.correct_option, options)
+
+    # Remove duplicates
+    unique_options = list(set(options))
+
+    # Check if correct_option is already in unique_options
+    if st.session_state.correct_option not in unique_options:
+        # If not, replace a random element with correct_option
+        replace_index = np.random.randint(0, len(unique_options))
+        unique_options[replace_index] = st.session_state.correct_option
+
+    # Select the first four unique options
+    final_options = unique_options[:4]
+    random.shuffle(final_options)
+    st.session_state.options = final_options
+
+    return
+
+
+if "random_index"  not in st.session_state:
+    st.session_state.random_index = np.random.randint(len(data))
+    print(st.session_state.random_index )
+
+    random_video_data = data.iloc[st.session_state.random_index]
+    st.session_state.url = random_video_data['url']
+
+    st.session_state.correct_option = random_video_data['sign']
+    # Randomly choose options
+    options = np.random.choice(unique_signs, size=4, replace=False)  # Choose 3 random options
+
+    # Concatenate correct option and randomly chosen options
+    options = np.append(st.session_state.correct_option, options)
+
+    # Remove duplicates
+    unique_options = list(set(options))
+
+    # Check if correct_option is already in unique_options
+    if st.session_state.correct_option not in unique_options:
+        # If not, replace a random element with correct_option
+        replace_index = np.random.randint(0, len(unique_options))
+        unique_options[replace_index] = st.session_state.correct_option
+
+    # Select the first four unique options
+    final_options = unique_options[:4]
+    random.shuffle(final_options)
+    st.session_state.options = final_options
+
+    print('im initializing')
+
+
+def submit_answer(option):
+
+    if option == st.session_state.correct_option:
+        st.write("<span style='color:green'>Correct!</span>", unsafe_allow_html=True)
+    else:
+        st.write(f"The correct option was: <span style='color:green'>{st.session_state.correct_option.capitalize()}</span>", unsafe_allow_html=True)
+        st.write(f"You selected: <span style='color:red'>{option.capitalize()}</span>", unsafe_allow_html=True)
+
+    st.session_state.answer_submitted = True
+
 
 
 # Function to display the game options
-def display_game(video_data):
+def display_game():
     st.subheader("Watch the Sign")
-    st.video(video_data['url'] )
+    st.video(st.session_state.url)
 
     st.subheader("Which Sign Is It?")
-    correct_option = video_data['sign']
-    options = generate_options(unique_signs, correct_option)
 
     # Display options in a 2x2 grid
     col1, col2 = st.columns(2)
 
-    # Loop through options and display buttons
-    for i, option in enumerate(options):
+    for i, option in enumerate(st.session_state.options):
         column = col1 if i < 2 else col2
-        with column:
-            if st.button(option.capitalize(), key=option, use_container_width=True):
-
-                # Update state to indicate that an option has been selected
-                st.session_state.option_selected = True
-                st.session_state.correct_option = correct_option
-
-                if option == correct_option:
-                    st.write("You got it right!")
-                else:
-                    st.write("Sorry, that's not correct. Try again!")
+        column.button(option.capitalize(), key=option, on_click=submit_answer,args=(option,), use_container_width=True)
 
 
 # Initialize session state
+if 'answer_submitted' not in st.session_state:
+    st.session_state.answer_submitted = False
 
-if 'option_selected' not in st.session_state:
-    st.session_state.option_selected = False
 
-if 'correct_option' not in st.session_state:
-    st.session_state.correct_option = None
-
+print(st.session_state.answer_submitted)
 
 # Check if an option has been selected
-if  st.session_state.option_selected:
+if st.session_state.answer_submitted:
+    time.sleep(2)
+    print('im inside the if statement')
     # If an option has been selected, display the correct option and reset state
-    if st.session_state.correct_option:
-        st.write(f"The correct option was: {st.session_state.correct_option.capitalize()}")
-    st.session_state.option_selected = False
+    st.session_state.answer_submitted = False
     st.session_state.correct_option = None
+    select_random_video()
+    display_game()
 
 else:
-    # If no option has been selected, play the game
-    random_video_data = select_random_video()
-    display_game(random_video_data)
+    display_game()
+    print('im at the bottom')
