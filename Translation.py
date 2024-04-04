293dc70d87e1ae3a24c95f817233d01b98452c2b
Benoit Frisque <benoitfrisque@gmail.com>
2024-04-04 09:29:10 +0200

    improved sidebar

--- Translation.py
+++ Translation.py
@@ -16,60 +16,13 @@ st.set_page_config(page_title="SignLens Demo",
 
             })
 
-# @st.cache_resource(allow_output_mutation=True)
-# def get_base64_of_bin_file(bin_file):
-#     with open(bin_file, 'rb') as f:
-#         data = f.read()
-#     return base64.b64encode(data).decode()
-
-# def set_png_as_page_bg(png_file):
-#     bin_str = get_base64_of_bin_file(png_file)
-#     page_bg_img = '''
-#     <style>
-#     body {
-#     background-image: url("data:image/png;base64,%s");
-#     background-size: cover;
-#     }
-#     </style>
-#     ''' % bin_str
-
-#     st.markdown(page_bg_img, unsafe_allow_html=True)
-#     return
-
-# set_png_as_page_bg('resources/background_signlens.png')
-
+# Constants
 NUM_CLASSES = 250
 
-# def set_background(png_file):
-#     bin_str = get_base64(png_file)
-#     page_bg_img = '''
-#     <style>
-#     .stApp {
-#     background-image: url("data:image/png;base64,%s");
-#     background-size: cover;
-#     }
-#     </style>
-#     ''' % bin_str
-#     st.markdown(page_bg_img, unsafe_allow_html=True)
-# set_background('resources/background_signlens.png')
-
-# url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
-
-# @st.cache_data
-# def get_img_as_base64(file):
-#     with open(file, "rb") as f:
-#         data = f.read()
-#     return base64.b64encode(data).decode()
-
-# img_background = get_img_as_base64("resources/background_signlens.png")
-
-#https://drive.google.com/file/d/1kzhZnny42X52bbAehiN94USuLKowmiJk/view?usp=drive_link
-#https://1drv.ms/i/s!AokgWCkazMPtpiqzTu31KLHslEj-?e=HNTSKG
 
 with open("resources/signlens-favicon-white.png", "rb") as image_file:
     encoded_string = base64.b64encode(image_file.read()).decode()
 side_logo = f"data:image/png;base64,{encoded_string}"
-background_image = "resources/background_signlens.png"
 
 # Add custom CSS
 st.markdown("""
@@ -95,55 +48,55 @@ h1 {
 </style>
 """, unsafe_allow_html=True)
 
-#url = "https://img.freepik.com/free-photo/sign-language-collage-design_23-2150528183.jpg?t=st=1711466807~exp=1711470407~hmac=c1c1a9a378d0a17254e6cf298fb262c2883e305f2ee08999e0771f76be98eeb4&w=900"
-#url = "resources/signlens-high-resolution-logo-black.png"
 
 st.markdown(
-        f"""
-        <style>
-            [data-testid="stSidebarNav"] + div {{
+    f"""
+    <style>
+        /* Adjust sidebar height based on its content */
+        .sidebar .sidebar-content {{
+            height: auto !important;
+            transition: height 0.5s;
+            position: relative; /* Make the sidebar content positioning relative */
+        }}
+
+        /* Text styling */
+        .about-text {{
+            text-align: center;
+            margin-top: 10px; /* Adjust margin-top to create space between text and image */
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+        }}
+
+        /* Logo styling */
+        [data-testid="stSidebarNav"] + div {{
                 position:absolute;
                 bottom: 0;
-                height:50%;
+                height: 50%;
+                width: 100%;
                 background-image: url({side_logo});
                 background-size: 50% auto;
                 background-repeat: no-repeat;
                 background-position-x: center;
                 background-position-y: bottom;
-                border-bottom: 2px solid black;
                 background-color: rgba(255, 255, 255, 0.08); /* Add this line for light background */
                 box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5); /* Add this line for shading */
             }}
-            body {{
-            background-image: url({background_image});
-            background-size: cover;
-            }}
-            [data-testid="stAppViewContainer"] > .main {{
-            background-image: url({background_image});
-            background-size: 100vw 100vh;  # This sets the size to cover 100% of the viewport width and height
-            background-position: center;
-            background-repeat: no-repeat;
-            }}
 
-                        .stApp > div:first-child {{
-                background-image: url({background_image});
-                background-size: cover;
-                background-position: center;
-                background-repeat: no-repeat;
-            }}
-            [data-testid="stAppViewContainer"] {{
-            background-color: transparent;
-            }}
-        </style>
-        """,
-        unsafe_allow_html=True,
-    )
+        /* Hide the image when sidebar is collapsed */
+        .sidebar.collapsed [data-testid="stSidebarNav"] + div {{
+            display: none;
+        }}
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
-
-# .stButton > button:hover {
-#     background-color: #c91a2c; #4caf50;
-#     color: purple;
-# }
+# Sidebar content
+logo = "resources/svg/logo-no-background.svg"
+st.sidebar.title("About SignLens")
+st.sidebar.caption("An app for translating sign language, but also aid in learning it. Upload a video of sign language gestures and click the button to translate the signs to text!")
 
 
 # Initialize the key in session state
@@ -157,21 +110,13 @@ def clicked(button):
     '''Update the value in session state when a button is clicked.'''
     st.session_state.clicked[button] = True
 
-# Sidebar content
-logo = "resources/svg/logo-no-background.svg"
-#st.sidebar.image(logo,use_column_width=True)
-st.sidebar.title("About SignLens")
-st.sidebar.caption("An app for translating sign language, but also aid in learning it. Upload a video of sign language gestures and click the button to translate the signs to text!")
-
-# Main content
-#st.title("SignLens Demo")
 
 col1, col2 = st.columns([2, 1])
 
 st.session_state.b2_disabled = True
 
 with col1:
-    st.image(logo,width=300)
+    st.image(logo, width=300)
     st.subheader("Upload a video to translate sign to text")
     holder = st.empty()
     title1 = st.empty()
@@ -190,21 +135,7 @@ with col1:
     if video:
         st.session_state.b2_disabled = False
         holder.video(video, start_time=0)
-        #st.session_state.url = video
-        # Use st.markdown to display the video
-        # st.markdown(f'''
-        #     <div style="position:relative; padding-top:56.25%;">
-        #         <iframe src="{st.session_state.url}?autoplay=1&loop=1" style="position:absolute; top:0; left:0; width:100%; height:100%;" frameborder="no" scrolling="no" allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen>
-        #         </iframe>
-        #     </div>
-        # ''', unsafe_allow_html=True)
         title1.write(":green[Video uploaded successfully!] :tada:")
-        # webrtc_streamer(
-        #     key="mute_sample",
-        #     video_html_attrs=VideoHTMLAttributes(
-        #         autoPlay=True, controls=True, style={"width": "100%"}, muted=muted
-        #     ),
-        # )
 
 with col2:
     with st.expander("Advanced settings", expanded=False):
@@ -240,7 +171,6 @@ with col2:
                                         frame_interval=frame_interval, #frame_limit=frame_limit,
                                         min_detection_confidence=min_detection_confidence,
                                         min_tracking_confidence=min_tracking_confidence)
-                    # st.json(json_landmarks, expanded=False) # just for debugging
 
                 with st.spinner("⏱️ Requesting API response... 🐢"):
                     #status_text.status("Requesting API response...")
@@ -259,19 +189,12 @@ with col2:
                     #result
                     sign = result['sign']
                     proba = result['probability']
-                    proba = round(100*(proba),2)
+                    proba = round(100*(proba), 2)
                     state = "complete"
                     time.sleep(1)
 
-
-                    # probc = ":%s[%s]" % (probability_color, f"{100*(proba):.2f}%")
-                    # #st.metric("probability",f"{100*(proba):.2f}%", probability_color)
                     st.title(sign)
-                    #st.write(:green["Translation guess"])
-                    #probability_color
-                    #"probability:"
-                    #st.metric(value=proba,label="probability")
-                    #st.text_area("Translation guess", sign, height=100)
+
                     probability_color = "green" if proba > 70 else "red" if proba < 50 else "yellow"
                     if probability_color == "green":
                         st.write("is our best guess with probability", f":green[{proba}%]")
@@ -281,8 +204,8 @@ with col2:
                         st.write("is our best guess with probability", f":red[{proba}%]")
 
                     search_term = result['sign'].split()[0]
-                    # Pixabay API
 
+                    # Pixabay API
                     if pixabay:
                         api_key = str(st.secrets.api_key)
                         url = "https://pixabay.com/api/?key=%s&q=%s&image_type=photo&pretty=true" % (str(api_key),search_term)
@@ -311,6 +234,6 @@ with col2:
                     state = "error"
 
             except Exception as e:
-                status_text.text(f"API Error: {e}")
-                print(f"API Error: {e}")
+                status_text.text(f"Error: {e}")
+                print(f"Error: {e}")
                 state = "error"
